0620b0615a3222dbb27c9b3cbb65e26d499b9762
Terentieva <myrka.t.1985@gmail.com>
2014-07-07 23:04:29 +0300

    first commit

---
+++ src/main/java/mavenproject/Hello_World.java
@@ -0,0 +1,11 @@
+package mavenproject;
+
+/**
+ * Created by Таня on 07.07.2014.
+ */
+public class Hello_World {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
